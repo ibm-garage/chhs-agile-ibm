edb9b2818f79d5c26d34994da57d7b8eb5c92771
Ankita Kulkarni <ankitadk@ca.ibm.com>
2016-06-03 18:18:58 -0400

    setup standalone selenium

--- conf.js
+++ conf.js
@@ -1,11 +1,12 @@
 // conf.js
 exports.config = {
   framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  //seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['spec/integration/**/*.js'],
   capabilities: {
     'browserName': 'phantomjs',
     'phantomjs.binary.path': require('phantomjs-prebuilt').path,
     'phantomjs.cli.args': ['--web-security=false', '--ignore-ssl-errors=true', '--webdriver-loglevel=DEBUG'],
-  }
+  },
+  seleniumServerJar: 'spec/support/selenium-server-standalone-2.53.0.jar'
 };
