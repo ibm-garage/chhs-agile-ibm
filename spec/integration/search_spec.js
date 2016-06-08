b785b1e1fe9f20506750437ebe46a1ff9bc27f73
Ankita Kulkarni <ankitadk@ca.ibm.com>
2016-06-08 11:59:20 -0400

    Remove a flaky tests

--- spec/integration/search_spec.js
+++ spec/integration/search_spec.js
@@ -16,12 +16,4 @@ describe('Search', function() {
     var myEle = element(by.cssContainingText('.material-icons', 'arrow_forward'));
     expect(myEle.isPresent()).toBe(true);
   });
-
-  it('clicks the search button', function() {
-    var zipCodes = element(by.model('zipcodes')).sendKeys('92553');
-    element(by.css('[ng-click="myForm.$valid && searchZipCodes()"]')).click();
-
-    var address = "12730 HEACOCK ST. SUITE 9";
-    expect(element(by.css('.address')).getText()).toContain(address);
-  });
 });
